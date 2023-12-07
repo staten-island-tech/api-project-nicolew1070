eac2470857b1554b31f0c9ea307023419d05e5b3
nicolew88 <nicolew88@nycstudents.net>
2023-12-07 13:13:02 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,7 +1,21 @@
-async function getData() {
-  let res = await fetch (
-    "https://api.lyrics.ovh/v1/artist/title"
-  );
-  let data = await res.json();
-  console.log(data);
+const URL = `https://api.quotable.io/random`
+
+async function getData(URL){
+    try {
+        const response = await fetch(URL);
+        console.log(response);
+        if (response.status !=200) {
+            throw new Error(response.statusText);
+        }
+        //take response from server and convert it to JSON
+        const data = await response.json();
+        console.log(data);
+        document.querySelector("h1").textContent = data.content;
+        document.querySelector("h2").textContent = data.author;
+    } catch (error) {
+        document.querySelector("h1").textContent = error;
+        document.querySelector("h1").textContent = 
+            "Please search for something else";
+    }
 }
+getData(URL);
