2718182c3ef505408147ae3e3e1f5e029b9a8e55
nicolew88 <nicolew88@nycstudents.net>
2023-12-07 19:35:12 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,21 +1,8 @@
-const URL = `https://api.quotable.io/random`
+const URL = `https://api.disneyapi.dev/character`
 
 async function getData(URL){
-    try {
         const response = await fetch(URL);
-        console.log(response);
-        if (response.status !=200) {
-            throw new Error(response.statusText);
-        }
-        //take response from server and convert it to JSON
         const data = await response.json();
         console.log(data);
-        document.querySelector("h1").textContent = data.content;
-        document.querySelector("h2").textContent = data.author;
-    } catch (error) {
-        document.querySelector("h1").textContent = error;
-        document.querySelector("h1").textContent = 
-            "Please search for something else";
-    }
 }
 getData(URL);
