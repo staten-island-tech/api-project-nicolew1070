c57a94d8f36b9ec517f5eef90a741371c64e561f
nicolew88 <nicolew88@nycstudents.net>
2023-12-20 13:12:39 -0500

    .

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,5 +1,9 @@
 const DOMSelectors = {
-
-}
+slytherin: document.querySelector(".slyth"),
+hufflepuff: document.querySelector(".huffle"),
+ravenclaw: document.querySelector(".raven"),
+gryffindor: document.querySelector(".gryff"),
+cards: document.querySelector(".display"),
+};
 
 export {DOMSelectors};
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,27 +1,42 @@
 //goal: cick buttons to see characters from the different houses of harry potter 
 import "../css/style.css";
+import { DOMSelectors } from "./dom";
 const URL = `https://hp-api.onrender.com/api/characters`
 //filters based on characters who are in Tangled -> gives the character name and film
 
+DOMSelectors.slytherin.addEventListener("click", function () {
+    if (document.body.classList.contains("freshy")) {
+      document.body.classList.add("minty");
+      document.body.classList.remove("freshy");
+    } else {
+      document.body.classList.add("freshy");
+      document.body.classList.remove("minty");
+    }
+  });  
+
 async function getData(URL){
-    try {
         const response = await fetch(URL);
         console.log(response);
-        if (response.status !=200) {
-            throw new Error(response.statusText);
-        }
-        //take response from server and convert it to JSON
-        const data = await response.json();
-        console.log(data);
-        document.querySelector("h1").textContent = data.name;
-        document.querySelector("h2").textContent = data.house;
-    } catch (error) {
-        document.querySelector("h1").textContent = "Harry Potter";
-    }
 }
-getData(URL);
 
-//click a button -> `https://hp-api.onrender.com/api/characters/house/Ravenclaw`
+function all () {
+    getData(URL).forEach((item) =>
+      DOMSelectors.cards.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card"> 
+      <h2 class="title">${item.name}</h2> 
+      <img class="display-img" src="${item.img}" alt="">
+      <h3>${item.house}</h3>
+      <h3>${item.calories}</h3>`
+      )
+    );
+  }
+  
+  DOMSelectors.ravenclaw.addEventListener("click", function (){
+    all ();
+    });
+
 // `https://hp-api.onrender.com/api/characters/house/hufflepuff`
 // `https://hp-api.onrender.com/api/characters/house/slytherin`
 // `https://hp-api.onrender.com/api/characters/house/gryffindor`
+// `https://hp-api.onrender.com/api/characters/house/ravenclaw`
