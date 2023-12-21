eb3dde435e644f891672cbb4b0d722a1f3c4132d
nicolew88 <nicolew88@nycstudents.net>
2023-12-21 12:24:22 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,9 +2,10 @@
 import "../css/style.css";
 import { DOMSelectors } from "./dom";
 const URL = `https://hp-api.onrender.com/api/characters`
+
 //filters based on characters who are in Tangled -> gives the character name and film
 
-DOMSelectors.slytherin.addEventListener("click", function () {
+/* DOMSelectors.slytherin.addEventListener("click", function () {
     if (document.body.classList.contains("freshy")) {
       document.body.classList.add("minty");
       document.body.classList.remove("freshy");
@@ -12,31 +13,35 @@ DOMSelectors.slytherin.addEventListener("click", function () {
       document.body.classList.add("freshy");
       document.body.classList.remove("minty");
     }
-  });  
-
-async function getData(URL){
-        const response = await fetch(URL);
-        console.log(response);
-}
-
-function all () {
-    getData(URL).forEach((item) =>
-      DOMSelectors.cards.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card"> 
-      <h2 class="title">${item.name}</h2> 
-      <img class="display-img" src="${item.img}" alt="">
-      <h3>${item.house}</h3>
-      <h3>${item.calories}</h3>`
-      )
-    );
-  }
-  
-  DOMSelectors.ravenclaw.addEventListener("click", function (){
-    all ();
-    });
-
+  });   */
 // `https://hp-api.onrender.com/api/characters/house/hufflepuff`
 // `https://hp-api.onrender.com/api/characters/house/slytherin`
 // `https://hp-api.onrender.com/api/characters/house/gryffindor`
 // `https://hp-api.onrender.com/api/characters/house/ravenclaw`
+
+async function getData(URL){
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  data.forEach((data)=> console.log(data))
+}
+getData(URL);
+
+function all () {
+  getData(URL).forEach((item) => DOMSelectors.cards.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card"> 
+    <h2 class="title">${item.name}</h2> 
+    <img class="display-img" src="${item.img}" alt="">
+    <h3>${item.house}</h3>
+    <h3>${item.gender}</h3>`
+    )
+  );
+}
+all();
+
+DOMSelectors.slytherin.addEventListener("click", function (){
+  DOMSelectors.card.innerHTML = '';
+  all ();
+  });
+   all();
