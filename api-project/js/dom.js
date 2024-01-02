f6943c3ac6f9232384ca58662979c7ea092c407a
nicolew88 <nicolew88@nycstudents.net>
2024-01-01 22:06:21 -0500

    .

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -3,6 +3,7 @@ underfive: document.querySelector("#five"),
 underten: document.querySelector("#ten"),
 underfifteen: document.querySelector("#fifteen"),
 undertwenty: document.querySelector("#twenty"),
+all: document.querySelector("#all"),
 cards: document.querySelector(".display"),
 };
 
--- api-project/js/index.js
+++ api-project/js/index.js
@@ -1,15 +1,16 @@
 //goal: cick buttons to see characters from the different houses of harry potter 
 import "../css/style.css";
 import { DOMSelectors } from "./dom";
-const URL = `https://www.cheapshark.com/api/1.0/deals?upperPrice=10&lowerPrice=5`;
+const URL = `https://www.cheapshark.com/api/1.0/deals?`;
+
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     if(response.status != 200) {
-      throw error (response);
+      throw new Error (response);
     } else {
       const data = await response.json();
-      displayCharacters(data); 
+      card(data); 
     }
     } catch (error) {
       console.log(error, "Invalid")
@@ -17,13 +18,30 @@ async function getData(URL) {
  }
  getData(URL);
 
-function displayCharacters(arr) {
-  arr.forEach((object) => DOMSelectors.cards.insertAdjacentHTML(
+function card(arr) {
+  arr.forEach((item) => DOMSelectors.cards.insertAdjacentHTML(
       "beforeend",
       `<div class="card"> 
-    <h2 class="title">${object.title}</h2> 
-    <img class="img" src="${object.thumb}" alt="">
-    <h3>Sale Price: ${object.salePrice}</h3>`
+    <h2 class="title">${item.title}</h2> 
+    <img class="img" src="${item.thumb}" alt="">
+    <h3>Sale Price: ${item.salePrice}</h3>
+    <h3>Original Price: ${item.normalPrice}</h3>`
     )
   );
 }
+
+let buttons = document.querySelectorAll(".btn")
+buttons.forEach((btn) => btn.addEventListener("click", function () {
+    let type = btn.textContent
+    let newArr = data.filter((item) => item.salePrice.includes(type))
+    DOMSelectors.cards.innerHTML = ""
+    card(newArr);
+  }))
+
+DOMSelectors.all.addEventListener("click", function () {
+  DOMSelectors.cards.innerHTML = ""
+  card();
+})
+
+//const hist = books.filter((books)=>books.genre.includes('historical'));
+//hist.forEach((books)=>console.log(books.name));
