ee3afd9721338d079897892bb50843393e67b99b
nicolew88 <nicolew88@nycstudents.net>
2024-01-02 13:11:42 -0500

    .

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,10 +1,16 @@
-const DOMSelectors = {
+/* const DOMSelectors = {
 underfive: document.querySelector("#five"),
 underten: document.querySelector("#ten"),
 underfifteen: document.querySelector("#fifteen"),
 undertwenty: document.querySelector("#twenty"),
 all: document.querySelector("#all"),
 cards: document.querySelector(".display"),
-};
+}; */
+
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    search: document.querySelector("#find"),
+    cards: document.querySelector(".display"),
+    };
 
 export {DOMSelectors};
--- api-project/js/index.js
+++ api-project/js/index.js
@@ -1,7 +1,47 @@
 //goal: cick buttons to see characters from the different houses of harry potter 
 import "../css/style.css";
 import { DOMSelectors } from "./dom";
-const URL = `https://www.cheapshark.com/api/1.0/deals?`;
+
+const URL = `https://api.disneyapi.dev/character?`;
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if(response.status != 200) {
+      throw new Error (response);
+    } else {
+      const data = await response.json();
+      card(data.data); 
+    }
+    } catch (error) {
+      console.log(error, "Invalid")
+      document.querySelector("h3").textContent = "Error. Film is unavailable."
+    }
+ }
+ getData(URL);
+
+function card(arr) {
+  arr.forEach((item) => DOMSelectors.cards.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card"> 
+    <h2 class="title">${item.name}</h2> 
+    <img class="img" src="${item.imageUrl}" alt="">
+    <h3></h3>`
+    )
+  );
+}
+
+/* let buttons = document.querySelectorAll(".btn")
+buttons.forEach((btn) => btn.addEventListener("click", function () {
+    let type = btn.textContent
+    let newArr = data.filter((item) => item.salePrice.includes(type))
+    DOMSelectors.cards.innerHTML = ""
+    card(newArr);
+  })) */
+
+//steam games api 
+
+/* const URL = `https://www.cheapshark.com/api/1.0/deals?`;
 
 async function getData(URL) {
   try {
@@ -36,12 +76,12 @@ buttons.forEach((btn) => btn.addEventListener("click", function () {
     let newArr = data.filter((item) => item.salePrice.includes(type))
     DOMSelectors.cards.innerHTML = ""
     card(newArr);
-  }))
+  })) */
 
-DOMSelectors.all.addEventListener("click", function () {
+/* DOMSelectors.all.addEventListener("click", function () {
   DOMSelectors.cards.innerHTML = ""
   card();
-})
+}) */
 
 //const hist = books.filter((books)=>books.genre.includes('historical'));
 //hist.forEach((books)=>console.log(books.name));
