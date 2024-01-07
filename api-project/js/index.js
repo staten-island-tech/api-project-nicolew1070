83a51dfd5679a301dbb6f69537cbd37d67b21aca
nicolew88 <nicolew88@nycstudents.net>
2024-01-07 18:18:07 -0500

    .

--- api-project/js/index.js
+++ api-project/js/index.js
@@ -1,7 +1,7 @@
 import "../css/style.css";
 import { DOMSelectors } from "./dom";
 
-const URL = `https://api.disneyapi.dev/character?pageSize=1000`;
+const URL = `https://api.disneyapi.dev/character?pageSize=1100`;  
 
 async function getData(URL) {
   try {
@@ -59,60 +59,3 @@ function thousand (arr) {
   clear();
   card(arr);
 })};
-
-
-
-/* let buttons = document.querySelectorAll(".btn")
-buttons.forEach((btn) => btn.addEventListener("click", function () {
-    let type = btn.textContent
-    let newArr = data.filter((item) => item.films.includes(type))
-    DOMSelectors.cards.innerHTML = ""
-    card(newArr);
-  })); */
-
-//steam games api 
-
-/* const URL = `https://www.cheapshark.com/api/1.0/deals?`;
-
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    if(response.status != 200) {
-      throw new Error (response);
-    } else {
-      const data = await response.json();
-      card(data); 
-    }
-    } catch (error) {
-      console.log(error, "Invalid")
-    }
- }
- getData(URL);
-
-function card(arr) {
-  arr.forEach((item) => DOMSelectors.cards.insertAdjacentHTML(
-      "beforeend",
-      `<div class="card"> 
-    <h2 class="title">${item.title}</h2> 
-    <img class="img" src="${item.thumb}" alt="">
-    <h3>Sale Price: ${item.salePrice}</h3>
-    <h3>Original Price: ${item.normalPrice}</h3>`
-    )
-  );
-}
-
-let buttons = document.querySelectorAll(".btn")
-buttons.forEach((btn) => btn.addEventListener("click", function () {
-    let type = btn.textContent
-    let newArr = data.filter((item) => item.salePrice.includes(type))
-    DOMSelectors.cards.innerHTML = ""
-    card(newArr);
-  })) */
-
-/* DOMSelectors.all.addEventListener("click", function () {
-  DOMSelectors.cards.innerHTML = ""
-  card();
-}) */
-
-//const hist = books.filter((books)=>books.genre.includes('historical'));
-//hist.forEach((books)=>console.log(books.name));
